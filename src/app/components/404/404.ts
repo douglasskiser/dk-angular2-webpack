ee8ab44e6f5490f0c360a20a2ba8ae0ddc31b395
douglasskiser <douglass.kiser@me.com>
2016-04-06 21:33:18 -0500

    remove unnecessary comment from 404 component

--- src/app/components/404/404.ts
+++ src/app/components/404/404.ts
@@ -2,7 +2,6 @@ import {Component} from 'angular2/core';
 
 @Component({
   selector: 'not-found',
-  //styles: [require('./home.scss')],
   template: require('./404.html')
 })
 export class NotFound {
